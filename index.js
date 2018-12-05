e3f99d733779145fef42a9de699acedbefc8f0a1
Mladen Paic <mladenpaic@Mladens-MacBook-Pro.local>
2018-12-05 15:53:19 +0100

    index script

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var Patient_1 = require("./src/Models/Patient");
+var patient1 = new Patient_1.Patient('Petar', 'Peric', 10923012, 123123);
+console.log(patient1);
---
+++ index.ts
@@ -0,0 +1,5 @@
+import {Patient} from "./src/Models/Patient";
+
+var patient1 = new Patient('Petar', 'Peric', 10923012, 123123);
+
+console.log(patient1);
