9855f1265e1525138c8a93874e99d15e03b6f037
Mladen Paic <paic.m@live.com>
2018-12-06 11:18:29 +0100

    doctor schedule exam

--- src/Models/Doctor.ts
+++ src/Models/Doctor.ts
@@ -1,4 +1,6 @@
-import {Person} from "./Person";
+import {Person}                from "./Person";
+import {Patient}               from "./Patient";
+import {LaboratoryExamination} from "./Exam/LaboratoryExamination";
 
 export class Doctor extends Person {
     readonly specialty: string;
@@ -12,9 +14,22 @@ export class Doctor extends Person {
     constructor(firstName: string, lastName: string, specialty: string) {
         super(firstName, lastName);
         this.specialty = specialty;
+
+        console.log(new Date(), 'Created doctor "' + firstName + '"');
     }
 
     public getSpecialty(): string {
         return this.specialty;
+    }
+
+    /**
+     *
+     * @param patient
+     * @param exam
+     */
+    public scheduleExam(patient: Patient, exam: LaboratoryExamination): void
+    {
+        exam.setDoctor(this);
+        patient.addLaboratoryExam(exam);
     }
 }
