38cbc8e6a87f00e9b5c4e3ba56f5ee62406802d9
Mladen Paic <paic.m@live.com>
2018-12-06 11:20:03 +0100

    creating patient, doctor, exams and adding exams to patient

--- index.ts
+++ index.ts
@@ -1,5 +1,21 @@
-import {Patient} from "./src/Models/Patient";
+import {Patient}                     from "./src/Models/Patient";
+import {Doctor}                      from "./src/Models/Doctor";
+import {BloodPressureExamFactory}    from "./src/Service/Factory/BloodPressureExamFactory";
+import {CholesterolLevelExamFactory} from "./src/Service/Factory/CholesterolLevelExamFactory";
+import {SugarLevelExamFactory}       from "./src/Service/Factory/SugarLevelExamFactory";
 
-var patient1 = new Patient('Petar', 'Peric', 10923012, 123123);
+let doctor = new Doctor('Milan', 'Milanovic', 'someSpecialty');
+let patient = new Patient('Dragan', 'Dragic', 2311987123321, 9172377);
+patient.setDoctor(doctor);
 
-console.log(patient1);
+let bloodPressureExamFactory = new BloodPressureExamFactory();
+let cholesterolLevelExamFactory = new CholesterolLevelExamFactory();
+let sugarLevelExamFactory = new SugarLevelExamFactory();
+
+let bloodPressureExam = bloodPressureExamFactory.createExam(new Date());
+let cholesterolLevelExam = cholesterolLevelExamFactory.createExam(new Date());
+let sugarLevelExam = sugarLevelExamFactory.createExam(new Date());
+
+doctor.scheduleExam(patient, bloodPressureExam);
+doctor.scheduleExam(patient, cholesterolLevelExam);
+doctor.scheduleExam(patient, sugarLevelExam);
